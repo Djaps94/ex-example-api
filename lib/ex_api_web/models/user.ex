174246434166ccf7ab1a274544af647e9da52c92
Predrag <djaps94@gmail.com>
2017-09-21 21:10:59 +0200

    Create user controller

--- lib/ex_api_web/models/user.ex
+++ lib/ex_api_web/models/user.ex
@@ -1,4 +1,8 @@
 defmodule ExApi.User do
+  @doc """
+  Module represents user data type with name, email and a password,
+  which maps to database table of the same name and vica versa.
+  """
   use Ecto.Schema
   import Ecto.Changeset
 
