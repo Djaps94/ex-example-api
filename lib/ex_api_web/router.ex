8daff5014a7a5db98b1d839ff3ab55ed96d591c0
Predrag <djaps94@gmail.com>
2017-09-21 21:26:04 +0200

    Define user routes

--- lib/ex_api_web/router.ex
+++ lib/ex_api_web/router.ex
@@ -13,14 +13,9 @@ defmodule ExApiWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", ExApiWeb do
-    pipe_through :browser # Use the default browser stack
+  scope "/api", ExApiWeb do
+    pipe_through :api
 
-    get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :create]
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", ExApiWeb do
-  #   pipe_through :api
-  # end
 end
