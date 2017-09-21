9c6381480139c3445771b4c83fd9869466825da8
Predrag <djaps94@gmail.com>
2017-09-21 20:12:45 +0200

    Generate create_users migration

---
+++ priv/repo/migrations/20170921180943_create_user.exs
@@ -0,0 +1,13 @@
+defmodule ExApi.Repo.Migrations.CreateUser do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :varchar
+      add :email, :varchar
+      add :password, :varchar
+
+      timestamps()
+    end
+  end
+end
