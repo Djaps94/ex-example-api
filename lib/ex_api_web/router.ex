4ccabd775728dd0b92e3adfb584dbc8cb486a73b
Predrag <djaps94@gmail.com>
2017-09-23 14:59:19 +0200

    Add bookmark routes

--- lib/ex_api_web/router.ex
+++ lib/ex_api_web/router.ex
@@ -16,6 +16,13 @@ defmodule ExApiWeb.Router do
   scope "/api", ExApiWeb do
     pipe_through :api
 
-    resources "/users", UserController, only: [:index, :create]
+    resources "/users", UserController, only: [:index, :create] do
+      resources "/bookmarks", BookmarkController, only: [:index,
+                                                        :create,
+                                                        :delete]
+    end
+
+    get("/users/:user_id/bookmarks/search", BookmarkController, :search)
+    get("/users/:user_id/bookmarks/check", BookmarkController, :check)
   end
 end
